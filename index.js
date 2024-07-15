6345054f705eb99442055fb16fb074a4193442d9
Joaquin Ortega <joaquin.blanco@bootcamp-upgrade.com>
2024-07-15 11:59:53 +0200

    update .env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const cors = require("cors")
+const cors = require('cors');
 
 // Importar rutas
 const userRoutes = require('./routes/user.routes');
@@ -14,17 +14,14 @@ const app = express();
 // Middleware para parsear JSON
 app.use(express.json());
 
-// Deshabilito CORS policy
-app.use(cors())
+// Deshabilitar CORS policy
+app.use(cors());
 
 // Conexión a MongoDB Atlas
 const MONGO_URI = process.env.MONGO_URI;
-mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log('Connected to MongoDB Atlas'))
-.catch(err => console.error('Error connecting to MongoDB', err));
+mongoose.connect(MONGO_URI)
+    .then(() => console.log('Connected to MongoDB Atlas'))
+    .catch(err => console.error('Error connecting to MongoDB', err));
 
 // Definir rutas
 app.use('/api/users', userRoutes);
@@ -34,12 +31,12 @@ app.use('/api/vehicles', vehicleRoutes);
 // Definir el puerto y poner en marcha el servidor
 const PORT = process.env.PORT || 3000;
 
-app.get("/",(req,res)=>{
-    res.send("Express en Vercel")
-})
+app.get("/", (req, res) => {
+    res.send("Express en Vercel");
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
 
-module.exports = app
+module.exports = app;
